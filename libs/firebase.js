1d654046209f5a6fe4b82007814672bd524d32e2
unknown <waliddkz1@gmail.com>
2021-12-03 22:39:03 -0800

    'upload'

--- libs/firebase.js
+++ libs/firebase.js
@@ -1,49 +1,39 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
-
-import config from '../config'
+import config from "../config";
 
 const {
-  firebase: {
-    enabled,
-    apiKey,
-    authDomain,
-    projectId,
-  },
-} = config
+  firebase: { enabled, apiKey, authDomain, projectId },
+} = config;
 
 const clientCredentials = {
   apiKey,
   authDomain,
   projectId,
-}
+};
 
-let firebaseApp
-let analytics
+let firebaseApp;
+let analytics;
 
 if (firebase.apps.length) {
-  firebaseApp = firebase.apps[0]
+  firebaseApp = firebase.apps[0];
 } else {
   if (enabled) {
-    firebaseApp = firebase.initializeApp(clientCredentials)
+    firebaseApp = firebase.initializeApp(clientCredentials);
   }
 }
-
-
 
 // auth start
 export const auth = firebase.auth();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 // auth end
 
+export default firebaseApp;
 
-
-
-export { firebaseApp, firebase, analytics, clientCredentials }
-export default firebaseApp
+export { firebaseApp, firebase, analytics, clientCredentials };
